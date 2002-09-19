22e303441cce03bd2f61b029de4ef0a66e79d321
Jonathan Stowe <jns@gellyfish.co.uk>
2002-09-19 14:37:04 +0000

    Import of JSTOWE/Acme-WheresYourDaddy-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-WheresYourDaddy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JSTOWE/Acme-WheresYourDaddy-0.01.tar.gz
    gitpan-cpan-author:       JSTOWE
    gitpan-cpan-maturity:     released

---
+++ WheresYourDaddy.pm
@@ -0,0 +1,41 @@
+package Acme::WheresYourDaddy;
+
+use 5.006;
+use strict;
+use warnings;
+
+
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+# Below is stub documentation for your module. You better edit it!
+
+=head1 NAME
+
+Acme::WheresYourDaddy - Perl extension to annoy Acme ;)
+
+=head1 SYNOPSIS
+
+  use Acme::WheresYourDaddy;
+
+=head1 DESCRIPTION
+
+This module doesn't do anything at all but it amused me to upload it.
+
+=head2 EXPORT
+
+None by default.
+
+
+=head1 AUTHOR
+
+Jonathan Stowe, E<lt>gellyfish@gellyfish.comE<gt>
+
+=head1 SEE ALSO
+
+L<perl>.
+
+=cut
